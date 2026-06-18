050b31922a02c073e23e50712b3a9111b231a909
SanjanaBR04 <sanjanabr041204@gmail.com>
2026-06-18 15:48:25 +0530

    new

---
+++ src/test/java/org/test/WebpageTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class WebpageTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+
+        driver.get("https://sanjanabr04.github.io/p--3/");
+    }
+
+    @Test
+    public void titleValidationTest() {
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Tripillar Solutions";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
